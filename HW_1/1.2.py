d9626b6f5c6e94976dee02f2a3a49f579fa43fe0
Maksym_Dodenko <majluc19@gmail.com>
2026-04-16 02:02:02 +0200

    homework_1 added

---
+++ HW_1/1.2.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, Flask!'
+
+@app.route('/user/<name>')
+def show_user_profile(name):
+    return f'Так держать, {name}'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
