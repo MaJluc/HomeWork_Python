2cc59469613eedb4729843d966d7d18b26831b56
Maksym_Dodenko <majluc19@gmail.com>
2026-04-25 22:58:58 +0200

    fix: исправлени по review

--- HW_1/1.2.py
+++ HW_1/1.2.py
@@ -1,13 +1,14 @@
 from flask import Flask
 
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return 'Hello, Flask!'
 
-@app.route('/user/<name>')
+
+@app.route('/user/<name>/')
 def show_user_profile(name):
     return f'Так держать, {name}'
 
